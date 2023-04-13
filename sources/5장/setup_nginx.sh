0c402d55f18523d1bbedea6e651a81dfddafeaa8
Dokyun Kim <36403352+steelflea@users.noreply.github.com>
2023-04-14 01:24:37 +0900

    Update setup_nginx.sh

--- sources/5장/setup_nginx.sh
+++ sources/5장/setup_nginx.sh
@@ -4,5 +4,4 @@ apt-get update
 apt-get -y install nginx
 
 # index.html 파일 만들기
-fileName=/var/www/html/index.html
-echo "Running ZION DEFENSE SYSTEM from host $(hostname)">${fileName}
+echo "Running ZION DEFENSE SYSTEM from host $(hostname)" > /var/www/html/index.html

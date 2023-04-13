5160166370e5f370221aed5643062d49789f9421
Dokyun Kim <36403352+steelflea@users.noreply.github.com>
2023-04-14 01:23:49 +0900

    Update setup_webbe_ubuntu.sh

--- sources/7장/setup_webbe_ubuntu.sh
+++ sources/7장/setup_webbe_ubuntu.sh
@@ -4,5 +4,4 @@ apt-get update
 apt-get -y install nginx
 
 # index.html 파일 만들기
-fileName=/var/www/html/index.html
-echo "Running Conversion API Web Service from host $(hostname)">${fileName}
+echo "Running Conversion API Web Service from host $(hostname)" > /var/www/html/index.html
